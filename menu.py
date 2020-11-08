5703ba5d46b07b2681f35b4e8d35167c55c2167a
Venkateshsandupatla <66814938+Venkateshsandupatla@users.noreply.github.com>
2020-11-08 23:00:43 +0530

    Add files via upload

--- menu.py
+++ menu.py
@@ -118,14 +118,73 @@ def LVMONFLY():
     os.system('lvextend --size {} /dev/{}'.format(size,lvm))
     os.system('sudo resize2fs {}'.format(lvm))
     dfht = os.system('df -hT')
-    print(dfht)     
+    print(dfht)       
+#-----------------------------Function for Ansible insatllation-----------
+def ansible():
 
-#------------------------Function for installing ansible-------------
-def ansible():    
+    os.system("tput setaf 4")
     os.system('sudo yum install python3 -y')  #to install python software
+    
+    os.system("tput setaf 5")
     os.system('sudo pip3 install ansible ') #to install ansible software
-    ans = os.system('ansible --version')
-    print(ansible)
+    
+    os.system("tput setaf 6")
+    os.system('ansible --version')
+#----------Function-Hadoop -----------
+
+#-------core_site file----------
+
+def core_site():
+    print('Enter NameNode IP Address :   ', end='')
+    NN_ip=input()
+   
+    os.system('echo \<configuration\> >> core-site.xml')
+    os.system('echo \<property\> >> core-site.xml')
+    os.system('echo \<name\>fs.default.name\<\/name\> >> core-site.xml')
+   
+    if cmd=='1':
+        os.system('echo \<value\>hdfs://{}:9001\<\/value\> >> core-site.xml'.format(NN_ip))
+    else:
+        os.system('echo \<value\>hdfs://{}:9001\<\/value\> >> core-site.xml'.format(NN_ip))
+   
+    os.system('echo \</property\> >> core-site.xml')
+    os.system('echo \<\/configuration\> >> core-site.xml')
+   
+    if cmd=='2':
+        os.system('scp core-site.xml {}:/etc/hadoop/core-site.xml'.format(remote_ip))
+    else:
+        os.system('cp core-site.xml /etc/hadoop/core-site.xml')
+
+#----Hdfs file-----------------------------    
+
+def hdfs_site():
+    
+    if cmd1=='6':
+        print('Enter DataNode Directory name you want to create :   ' ,end='')
+    
+    elif cmd1=='5':
+        print('Enter NameNode Directory name you want to create :   ' ,end='')
+    
+    dir_name=input()
+    
+    if cmd=='2':
+        os.system('ssh {} mkdir {}'.format(remote_ip , dir_name))
+    else:
+        os.system('mkdir {}'.format(dir_name))
+    os.system('echo \<configuration\> >> hdfs-site.xml')
+    os.system('echo \<property\> >> hdfs-site.xml')
+    
+    if cmd1=='6':
+        os.system('echo \<name\>dfs.data.dir\<\/name\> >> hdfs-site.xml')
+    elif cmd1=='5':
+        os.system('echo \<name\>dfs.name.dir\<\/name\> >> hdfs-site.xml')
+    os.system('echo \<value\>{}\<\/value\> >> hdfs-site.xml'.format(dir_name))
+    os.system('echo \</property\> >> hdfs-site.xml')
+    os.system('echo \<\/configuration\> >> hdfs-site.xml')
+    if cmd=='2':
+        os.system('scp hdfs-site.xml {}:/etc/hadoop/hdfs-site.xml'.format(remote_ip))
+    else:
+        os.system('cp hdfs-site.xml /etc/hadoop/hdfs-site.xml')
 #-----------------------------------------------------------Functions for remote vms------------------------------------------#
 
 #------------function for configuring docker--------------
@@ -251,7 +310,23 @@ while True:
            container()
         elif '4' in cmd1:
            ansible()
-           
+        elif '5' in cmd1:
+           os.system('rpm -ivh /root/hadoop-1.2.1-1.x86_64.rpm --force')
+           os.system('rpm -ivh /root/jdk-8u171-linux-x64.rpm')
+           hdfs_site()
+           core_site()
+           os.system('hadoop namenode -format')
+           os.system('hadoop-daemon.sh start namenode')
+           jps = os.system('jps')
+           print(jps)
+        elif '6' in cmd1:
+           os.system('rpm -ivh /root/hadoop-1.2.1-1.x86_64.rpm --force')
+           os.system('rpm -ivh /root/jdk-8u171-linux-x64.rpm')
+           hdfs_site()
+           core_site()
+           os.system('hadoop-daemon.sh start datanode')
+           jp = os.system('jps')
+           print(jp)
         elif '7' in cmd1:
            LVM()
         elif '8' in cmd1:
